40f1a4e8ac81ba90ad3bd432c91ad19fbfa2e987
Scott Morrison <scott.morrison@gmail.com>
2020-06-27 19:59:16 +1000

    an easier proof

--- src/data/polynomial_cayley_hamilton.lean
+++ src/data/polynomial_cayley_hamilton.lean
@@ -20,7 +20,7 @@ lemma foo {p : polynomial R} {r : R} {a : ℕ} :
   coeff (p * (X - monomial 0 r)) (a + 1) = coeff p a - coeff p (a + 1) * r :=
 sorry
 
--- @[simp] lemma quux {p : polynomial R} : p.coeff (p.nat_degree + 1) = 0 := sorry
+@[simp] lemma quux {p : polynomial R} : p.coeff (p.nat_degree + 1) = 0 := sorry
 
 lemma sum_over_range' (p : polynomial R) {f : ℕ → R → S} (h : ∀ n, f n 0 = 0)
   (n : ℕ) (w : p.nat_degree < n) :
@@ -50,25 +50,7 @@ sum_over_range' p h (p.nat_degree + 1) (lt_add_one _)
 by rw [monomial_eq_smul_X, coeff_smul, coeff_X_pow, if_pos rfl, mul_one]
 
 @[simp] lemma mul_coeff_zero (p q : polynomial R) : coeff (p * q) 0 = coeff p 0 * coeff q 0 :=
-begin
-  apply polynomial.induction_on' p,
-  { intros φ ψ hφ hψ, rw [add_mul, coeff_add, coeff_add, add_mul, hφ, hψ], },
-  intros m r,
-  apply polynomial.induction_on' q,
-  { intros φ ψ hφ hψ, rw [mul_add, coeff_add, coeff_add, mul_add, hφ, hψ], },
-  intros n s,
-  rw [monomial_eq_smul_X, monomial_eq_smul_X, ← C_mul', ← C_mul', X_pow_mul_assoc, ← mul_assoc,
-      ← C_mul, mul_assoc, ← pow_add, coeff_C_mul, coeff_C_mul, coeff_C_mul],
-  simp only [if_t_t, mul_boole, coeff_X_pow, zero_mul, ite_mul, mul_ite, mul_zero],
-  cases n,
-  { simp only [mul_one, nat.nat_zero_eq_zero, if_true, eq_self_iff_true, zero_add],
-    split_ifs; refl },
-  cases m,
-  { simp only [mul_one, if_true, eq_self_iff_true], },
-  rw [if_neg, if_neg],
-  { exact (nat.succ_ne_zero n).symm },
-  { simp only [nat.succ_eq_add_one, ←add_assoc], exact (nat.succ_ne_zero _).symm }
-end
+by simp [coeff_mul]
 
 lemma eval₂_mul_X_sub_monomial {p : polynomial R} {r : R} :
   (p * (X - monomial 0 r)).eval₂ f (f r) = 0 :=
